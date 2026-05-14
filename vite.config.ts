78749b7c29917570dd553f7f4c234ae74d70e39b
unknown <aniduyakubu@yahoo.com>
2026-05-14 19:47:52 +0100

    fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,27 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+
 	build: {
 		target: 'es2020',
 		cssCodeSplit: true,
+
 		rollupOptions: {
 			output: {
-				manualChunks: {
-					three: ['three'],
-					gsap: ['gsap']
+				manualChunks(id) {
+					if (id.includes('gsap')) {
+						return 'gsap';
+					}
+
+					// REMOVE three chunking
+					// if (id.includes('three')) {
+					// 	return 'three';
+					// }
 				}
 			}
 		}
 	},
+
 	server: {
 		fs: { strict: false }
 	}
